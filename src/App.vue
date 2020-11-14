5651faa71bb1c28ad0d6d76140e393dc574805bd
Antonio Cunanan <antonio.cunanan@gmail.com>
2020-11-14 17:19:48 -0600

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar @term-change="onTermChange"></SearchBar>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      console.log({searchTerm});
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
